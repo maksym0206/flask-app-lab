6ef4aa107d97cd8644c7d3a1db37e2bdd6a77ed6
Maksym_Sliusarchuk <maksym.sliusarchuk.22@pnu.edu.ua>
2024-11-25 22:16:09 +0200

    done lab5

--- app/views.py
+++ app/views.py
@@ -3,6 +3,15 @@ from . import app
 
 @app.route('/')
 def main():
-    return render_template("base.html")
+    return render_template("index.html")
+
+@app.route('/homepage') 
+def home():
+    """View for the Home page of your website."""
+    agent = request.user_agent
+
+    return render_template("home.html", agent=agent)
+
+
 
 
